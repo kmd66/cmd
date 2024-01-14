dee4916acdc0766e07d61282023eff105d8dc6db
kamatech-ir <98743650+kamatech-ir@users.noreply.github.com>
2024-01-14 09:31:42 +0330

    .

--- CMS/Pages/Inside/Menu/MenuHelper.cs
+++ CMS/Pages/Inside/Menu/MenuHelper.cs
@@ -39,7 +39,7 @@ namespace CMS.Pages.Inside.Menu
             var result = await _dataSource.AddAsync(model);
             if (!result.Success)
                 return Result.Failure(message: result.Message);
-
+            model.Date = DateTime.Now;
             await SetOrder(model.Parent);
 
             return Result.Successful();
